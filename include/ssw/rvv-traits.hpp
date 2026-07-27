b87f6b583b8dc7dfc0e47583a90b2d50066b0706
AlexanderGSC <alexander.gsc@gmail.com>
2026-07-27 19:41:21 +0200

    added traits for LMUL=8

--- include/ssw/rvv-traits.hpp
+++ include/ssw/rvv-traits.hpp
@@ -234,7 +234,80 @@ template <> struct rvv_traits<int16_t, 4>
 };
 
 
-template <> struct rvv_traits<int16_t, 8> {};
+template <> struct rvv_traits<int16_t, 8> 
+{
+    using vector_t = vint16m8_t;    
+    using elem_t   = int16_t;    
+    using index_v_t= vuint8m4_t;
+    using index_t  = uint8_t;
+
+    static inline size_t setvl(size_t n) 
+        {return __riscv_vsetvl_e16m8(n);}
+    
+    static inline vector_t load(elem_t* p, size_t vl) 
+        {return __riscv_vle16_v_i16m8(p,vl);}
+    
+    static inline vector_t move(elem_t v, size_t vl)  
+        {return __riscv_vmv_v_x_i16m8(v,vl);}
+
+    static inline vector_t slideup_zero(vector_t v, size_t offset, size_t vl) {
+        vector_t zeros = __riscv_vmv_v_x_i16m8(0,vl);
+        return __riscv_vslideup_vx_i16m8(zeros, v, offset, vl);}
+
+    static inline vector_t loxe(const elem_t* p, index_v_t i, size_t vl)
+        {return __riscv_vloxei8_v_i16m8(p, i, vl);}
+
+    static inline index_v_t ldse(const index_t* p, size_t stride, size_t vl) 
+        {return __riscv_vlse8_v_u8m4(p,static_cast<ptrdiff_t>(stride),vl);}
+
+    static inline vector_t add(vector_t v1, vector_t v2, size_t vl) 
+        {return __riscv_vadd_vv_i16m8(v1,v2,vl);}
+
+    static inline vector_t add(vector_t v, elem_t i, size_t vl) 
+        {return __riscv_vadd_vx_i16m8(v,i,vl);}
+
+    static inline vector_t addsat(vector_t v1, vector_t v2, size_t vl)
+        {   vector_t sum = __riscv_vsadd_vv_i16m8(v1,v2,vl);
+            return __riscv_vmax_vx_i16m8(sum,0,vl);}
+
+    static inline vector_t max(vector_t v1, vector_t v2, size_t vl)
+        {   v1 = __riscv_vmax_vv_i16m8(v1,v2,vl);
+            return __riscv_vmax_vx_i16m8(v1,0,vl);}
+
+    static inline long max_idx(vector_t v1, elem_t val, size_t vl) {
+        vbool2_t max_mask = __riscv_vmseq_vx_i16m8_b2(v1,val,vl);
+        return __riscv_vfirst_m_b2(max_mask, vl);}
+
+    static inline elem_t max(vector_t v, size_t vl) {
+            vint16m1_t red_dest = __riscv_vmv_v_x_i16m1(INT16_MIN,1);
+            red_dest = __riscv_vredmax_vs_i16m8_i16m1(v,red_dest,vl);
+            return __riscv_vmv_x_s_i16m1_i16(red_dest);}
+
+    static inline vector_t sll(vector_t v, size_t i, size_t vl) 
+        {return __riscv_vsll_vx_i16m8(v,i,vl);}
+
+    static inline index_v_t sll(index_v_t v, size_t i, size_t vl)
+        {return __riscv_vsll_vx_u8m4(v,i,vl);}
+
+    static inline void store(elem_t* p, vector_t v, size_t vl) 
+        {return __riscv_vse16_v_i16m8(p,v,vl);}
+
+    static inline bool any_greater(vector_t v1, elem_t* p, elem_t g, size_t vl) 
+    {
+        vector_t v2    = __riscv_vle16_v_i16m8(p, vl);
+        v2             = __riscv_vadd_vx_i16m8(v2, g, vl);
+        vbool2_t mask = __riscv_vmsgt_vv_i16m8_b2(v1, v2, vl);
+        long first_idx = __riscv_vfirst_m_b2(mask,vl);
+        return (first_idx != -1);      
+    }
+    
+    static inline void print_score(vector_t v, size_t vl) {
+        std::vector<elem_t> aux(vl,0);
+        __riscv_vse16_v_i16m8(aux.data(),v,vl);
+        for (elem_t s : aux) std::cout << s << " ";
+        std::cout << std::endl;
+    }
+};
 
 
 template <> struct rvv_traits<int32_t, 1> {
